a3bec74679aa9bb034e61dc8c3596ab35704872e
asrospie <asrospierski@gmail.com>
2020-12-17 19:04:54 -0500

    Conversion to sapper

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,42 +1,60 @@
-<style>
-</style>
-
 <script>
-    import {
-        Button, Collapse, Nav, Navbar, NavbarBrand, NavbarToggler,
-        NavItem, NavLink,
-    } from 'sveltestrap'; 
-    import Icon from 'svelte-awesome';
-    import { home, code } from 'svelte-awesome/icons';
-
-    const style = "color: white;";
-    let isOpen = false;
-
-    function handleUpdate(event) {
-        isOpen = event.detail.isOpen;
-    }
-
+	export let segment;
 </script>
 
-<Navbar expand="md" dark color="dark" sticky="top">
-    <NavbarBrand href="/">Alec Rospierski</NavbarBrand>
-    <NavbarToggler on:click={() => {isOpen = !isOpen}} />
-    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-        <Nav class="ml-auto" navbar>
-            <NavItem>
-                <NavLink href="/">Home</NavLink>
-                <!-- <a class="nav-link" href="/"><Icon data={home} {style}/>Home</a> -->
-            </NavItem>
-            <!-- TODO -->
-            <!-- <NavItem>
-                <NavLink href=".">Blog</NavLink>
-            </NavItem> -->
-            <NavItem>
-				<NavLink href="#/dndtools">D&D Tools</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="https://github.com/asrospie/">GitHub</NavLink>
-            </NavItem>
-        </Nav>
-    </Collapse>
-</Navbar>
+<style>
+	nav {
+		border-bottom: 1px solid rgba(255,62,0,0.1);
+		font-weight: 300;
+		padding: 0 1em;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	/* clearfix */
+	ul::after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+
+	li {
+		display: block;
+		float: left;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: rgb(255,62,0);
+		display: block;
+		bottom: -1px;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+</style>
+
+<nav>
+	<ul>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+
+		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+		     the blog data when we hover over the link or tap it on a touchscreen -->
+		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+	</ul>
+</nav>
